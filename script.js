38aa262e3a85825de3b96e2792eccd63d9f786ca
MaianneThornton <maianne.thornton@gmail.com>
2021-09-27 12:00:00 -0400

    Corrected in class

--- script.js
+++ script.js
@@ -3,27 +3,43 @@ let randomNum = Math.floor(Math.random() * 5) + 1;
 console.log(randomNum);
 
 //1b
-randomNum = Math.floor(Math.random() * 5) + 1;
 if (randomNum >= 4) {
   console.log("Greater than or equal to 4");
-} else if (randomNum === 2 || 3) {
+} else if (randomNum === 2 || randomNum === 3) {
   console.log("Equal to 2 or 3");
 } else {
   console.log("Equal to 1");
 }
+// Another Way to Solve:
+// if (randomNum >= 4) {
+//   // Same as line above
+// // if (randomNum > 3) {
+//   console.log("Greater than or equal to 4");
+// } else if (randomNum >= 2) {
+// // Same as line above
+// // } else if (randomNum > 1) {
+//   console.log("Equal to 2 or 3");
+// } else {
+//   console.log("Equal to 1");
+// }
 
 //1c
-if (randomNum != 3) {
+if (randomNum !== 3) {
   console.log("Not equal to 3");
 }
 
 //1d
-if (randomNum === 3 || 5) {
+if (randomNum !== 3 && randomNum !== 5) {
   console.log("NOT equal to 3 AND not equal to 5");
 }
+// Another Example for 1d
+
+// if (randomNum === 1 || randomNum === 2 || randomNum === 4) {
+//     console.log("NOT equal to 3 AND not equal to 5");
+//   }
 
 // 1e
-if (randomNum === 2 || 4) {
+if (randomNum === 2 || randomNum === 4) {
   console.log("Equal to 2 OR equal to 4");
 }
 
@@ -32,23 +48,28 @@ if (randomNum === 2 || 4) {
 console.log(randomNum >= 4 ? "Greater than or equal to 4" : "Less than 4");
 
 //3
+// Switch Statement takes in a parameter/argument and returns something based on the CASE (Or the match) that the parameter/argument goes with
+// In this example if randomNum is 1 it will run the console.log("One"); code, etc.
 switch (randomNum) {
   case 1:
-    randomNum = 1;
+    // randomNum = 1;
     console.log("One");
+    // Break keywords are needed between each case so that the code for the cas that follows is NOT run as well
     break;
-    case 2:
-    randomNum = 2;
+  case 2:
+    // randomNum = 2;
     console.log("Two");
     break;
-    case 3:
-    randomNum = 3;
+  case 3:
+    // randomNum = 3;
     console.log("Three");
     break;
-    case 4:
-    randomNum = 4;
+  case 4:
+    // randomNum = 4;
     console.log("Four");
     break;
-    default:
+  default:
     console.log("Five");
+  // Default works like ELSE in a Conditional Statement (If the value does not match any of the cases,. then it will run what is in the default)
+  // There is no need for a BREAK at the end of the Switch Statement since there are no other cases/possibilities
 }
